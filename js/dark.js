80cd1aa3717abedfda16656740b8ad880a6f3f16
Guru_rl <designeronmission@gmail.com>
2026-01-02 19:01:38 +0530

    content & design changes 2nd jan

---
+++ js/dark.js
@@ -0,0 +1,15 @@
+
+/* Theme based js */
+
+
+  const html = document.documentElement;
+  const savedTheme = localStorage.getItem("theme") || "light";
+  html.setAttribute("data-theme", savedTheme);
+
+  document.getElementById("themeToggle").onclick = () => {
+    const theme =
+      html.getAttribute("data-theme") === "dark" ? "light" : "dark";
+    html.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  };
+
